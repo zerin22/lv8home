0a3b103e5c6ece72f571b077155a578eaa97f1ad
Tajrin Zerin <tajrin.ini@gmail.com>
2021-07-31 16:58:34 +0600

    HomePage+About+Contact+navBar with bootstrap+routing+nameRoute

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,11 @@
+<section class="bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <a href="{{ route('homepage') }}">Home</a>
+                <a href="{{ route('about') }}">About</a>
+                <a href="{{ route ('contact') }}">Contact</a>
+            </div>
+        </div>
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('homepage');
+
+Route::get('about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('contact',function(){
+    return view('contact');
+})->name('contact');
